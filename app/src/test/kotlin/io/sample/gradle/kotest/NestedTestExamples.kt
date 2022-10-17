84d6146f231890ccf297bf9b9e6851ae6df6bc64
Miguel Aranha Baldi Horlle <miguel_horlle@terceiros.sicredi.com.br>
2022-10-17 15:45:48 -0300

    Sample project Gradle 7.5.1 + Kotest 5.5.0

---
+++ app/src/test/kotlin/io/sample/gradle/kotest/NestedTestExamples.kt
@@ -0,0 +1,19 @@
+package io.sample.gradle.kotest
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class NestedTestExamples : DescribeSpec({
+
+    describe("an outer test") {
+
+        it("an inner test") {
+            1 + 2 shouldBe 3
+        }
+
+        it("an inner test too!") {
+            3 + 4 shouldBe 7
+        }
+    }
+
+})
